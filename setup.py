fa90e2c34596dd77d29ebf8c931c4299dd0f9ce0
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-03-27 16:55:48 +0100

    RSTify docs

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     name="yaco.structuralcontent",
     version=version,
     description="Structural Content",
-    long_description=open("README.txt").read()
+    long_description=open("README.rst").read()
     + "\n"
-    + open(os.path.join("docs", "HISTORY.txt")).read(),
+    + open(os.path.join("docs", "HISTORY.rst")).read(),
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
