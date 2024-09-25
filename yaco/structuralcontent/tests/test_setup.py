9ff42992cc62d113281d7a73fbb0c39156465a8b
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-09-25 12:29:33 +0200

    fix tests

--- yaco/structuralcontent/tests/test_setup.py
+++ yaco/structuralcontent/tests/test_setup.py
@@ -31,7 +31,7 @@ class TestSetup(unittest.TestCase):
 
     def test_product_installed(self):
         """Test if yaco.structuralcontent is installed."""
-        self.assertTrue(self.installer.isProductInstalled("yaco.structuralcontent"))
+        self.assertTrue(self.installer.is_product_installed("yaco.structuralcontent"))
 
     def test_browserlayer(self):
         """Test that IYacoStructuralcontent is registered."""
@@ -53,12 +53,12 @@ class TestUninstall(unittest.TestCase):
             self.installer = api.portal.get_tool("portal_quickinstaller")
         roles_before = api.user.get_roles(TEST_USER_ID)
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
-        self.installer.uninstallProducts(["yaco.structuralcontent"])
+        self.installer.uninstall_product(["yaco.structuralcontent"])
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if yaco.structuralcontent is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled("yaco.structuralcontent"))
+        self.assertFalse(self.installer.is_product_installed("yaco.structuralcontent"))
 
     def test_browserlayer_removed(self):
         """Test that IYacoStructuralcontent is removed."""
